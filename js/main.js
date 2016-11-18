d32f876ff40f536e9d50807dc760e7a283b737ae
sufiyan <sufiyan@nisostech.com>
2016-11-18 17:14:35 +0530

    button menu

--- js/main.js
+++ js/main.js
@@ -8,5 +8,31 @@
             strings: ["First sentence.", "Second sentence."],
             typeSpeed: 0
         });
+        
+     
+
+        var button = document.getElementById('cn-button'),
+        wrapper = document.getElementById('cn-wrapper');
+
+        //open and close menu when the button is clicked
+        var open = false;
+        button.addEventListener('click', handler, false);
+
+        function handler(){
+          if(!open){
+            this.innerHTML = "-";
+            classie.add(wrapper, 'opened-nav');
+          }
+          else{
+            this.innerHTML = "+";
+            classie.remove(wrapper, 'opened-nav');
+          }
+          open = !open;
+        }
+        function closeWrapper(){
+            classie.remove(wrapper, 'opened-nav');
+        }
+
+
     });
 })();
